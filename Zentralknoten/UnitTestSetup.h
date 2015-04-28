ec2ab0712c8d1e08bf5e4ca0f0ca7532b8675d11
burningT <tobias-fr.rastetter@gmx.de>
2015-04-28 11:27:15 +0200

    Headerfile für Unit-Tests

---
+++ Zentralknoten/UnitTestSetup.h
@@ -0,0 +1,10 @@
+#ifndef UNIT_TEST_SETUP_H
+#define UNIT_TEST_SETUP_H 1
+
+/**
+* Headerfile für Aktivierung der Unit-Test-Einstellungen
+**/
+
+#define UNIT_TEST 1		//Falls 1 sind Unit-Tests aktiviert
+
+#endif
