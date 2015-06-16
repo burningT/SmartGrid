8f8cb89e9b4d9780508ea2a61093b02d58bbde43
burningT <tobias-fr.rastetter@gmx.de>
2015-06-16 13:02:38 +0200

    Update und Erweiterung der Datenhaltung des Servers
    -GroupData wurde korrigiert und um Destruktor erweitert
    -StringConverter wurde für ints erweitert, kommentiert und mit
    Destruktor erweitert
    -TariffData wurde kommentiert und mit Destruktor erweitert
    -TestMain wurde angelegt
    -ZentralknotenData wurde erstellt

--- SmartGridServer/SmartGridServer/StringConverter.cpp
+++ SmartGridServer/SmartGridServer/StringConverter.cpp
@@ -9,19 +9,44 @@
 */
 #include "StringConverter.h"
 
+/** Dies ist ein Konstruktor
+*
+* Die Variablen der Klasse erhalten hier die folgenden Werte:
+*	delimiterL = "[";
+*	delimiterR = "]";
+*	separator  = ",";
+*
+*/
 StringConverter::StringConverter(){
 	delimiterL = "[";
 	delimiterR = "]";
 	separator  = ",";
 }
+
+/** Dies ist ein Konstruktor
+*
+* Die Variablen der Klasse erhalten hier die folgenden Werte:
+*	delimiterL = delimiterL;
+*	delimiterR = delimiterR;
+*	separator  = separator;
+*
+*/
 StringConverter::StringConverter(string delimiterL, string delimiterR, string separator){
 	StringConverter::delimiterL = delimiterL;
 	StringConverter::delimiterR = delimiterR;
 	StringConverter::separator = separator;
 }
+/** Dies ist ein Destruktor
+*
+*/
+StringConverter::~StringConverter(){
+	delimiterL = "";
+	delimiterR = "";
+	separator = "";
+}
 
 /**
-*	Diese Methode konvertiert einen Vektor in einen String
+*	Diese Methode konvertiert einen Vektor mit floats in einen String
 */
 string StringConverter::toString(vector<float> vector){
 
@@ -31,6 +56,25 @@ string StringConverter::toString(vector<float> vector){
 
 		oss << delimiterL;	//Füge linken delimiter ein
 														//Füge Elemente mit Trennzeichen ein
+		copy(vector.begin(), vector.end() - 1, ostream_iterator<int>(oss, separator.c_str()));
+
+		oss << vector.back() << delimiterR;		//Füge letztes Element mit rechtem delimiter ein
+	}
+
+	return oss.str();
+}
+
+/**
+*	Diese Methode konvertiert einen Vektor mit ints in einen String
+*/
+string StringConverter::toString(vector<int> vector){
+
+	ostringstream oss;
+
+	if (!vector.empty()){
+
+		oss << delimiterL;	//Füge linken delimiter ein
+		//Füge Elemente mit Trennzeichen ein
 		copy(vector.begin(), vector.end() - 1, ostream_iterator<int>(oss, separator.c_str()));
 
 		oss << vector.back() << delimiterR;		//Füge letztes Element mit rechtem delimiter ein
--- SmartGridServer/SmartGridServer/TariffData.cpp
+++ SmartGridServer/SmartGridServer/TariffData.cpp
@@ -10,41 +10,82 @@
 
 #include "TariffData.h"
 
+/** Dies ist ein Konstruktor
+*
+* Die Variablen der Klasse erhalten hier die folgenden Werte:
+*	identifier = 0;
+*	tariffCourse.resize(COURSE_LEN);
+*	networkLoadCourse.resize(COURSE_LEN);
+*
+*/
 TariffData::TariffData(){
 	identifier = 0;
 	tariffCourse.resize(COURSE_LEN);
 	networkLoadCourse.resize(COURSE_LEN);
 }
-
+/** Dies ist ein Konstruktor
+*
+* Die Variablen der Klasse erhalten hier die übergebenen Werte:
+*	identifier = identifier;
+*	tariffCourse = tariffCourse;
+*	networkLoadCourse = networkLoadCourse;
+*
+*/
 TariffData::TariffData(int identifier, vector<float>  tariffCourse, vector<float> networkLoadCourse){
 	SetIdentifier(identifier);
 	SetTariffCourse(tariffCourse);
 	SetNetworkLoadCourse(networkLoadCourse);
 }
+/** Dies ist ein Destruktor
+*
+*/
+TariffData::~TariffData(){
+	identifier = 0;
+	tariffCourse.clear();
+	networkLoadCourse.clear();
+}
 
+/** Diese Methode setzt identifier auf den Wert der Übergabe
+*
+*/
 void TariffData::SetIdentifier(int identifier){
 
 	TariffData::identifier = identifier;
 }
+/** Diese Methode gibt den Wert von identifier zurück
+*
+*/
 int TariffData::GetIdentifier(){
 	
 	return identifier;
 }
+/** Diese Methode gibt den Wert von identifier als String zurück
+*
+*/
 string TariffData::GetIdentifier_string(){
 
 	return to_string(identifier);
 }
 
+/** Diese Methode setzt tariffCourse auf den Wert der Übergabe
+*
+*/
 void TariffData::SetTariffCourse(vector<float> tariffCourse){
 	
 	if (tariffCourse.capacity() == COURSE_LEN){
 		TariffData::tariffCourse = tariffCourse;
 	}
 }
+/** Diese Methode gibt den Wert von GetTariffCourse zurück
+*
+*/
 vector<float>  TariffData::GetTariffCourse(){
 
 	return tariffCourse;
 }
+/** Diese Methode gibt den Wert von GetTariffCourse als String zurück
+*
+*/
 string	TariffData::GetTariffCourse_string(){
 
 	StringConverter converter;
@@ -52,15 +93,24 @@ string	TariffData::GetTariffCourse_string(){
 	return converter.toString(tariffCourse);
 }
 
+/** Diese Methode setzt tariffCourse auf den Wert der Übergabe
+*
+*/
 void TariffData::SetNetworkLoadCourse(vector<float> networkLoadCourse){
 
 	if (networkLoadCourse.capacity() == COURSE_LEN){
 		TariffData::networkLoadCourse = networkLoadCourse;
 	}
 }
+/** Diese Methode gibt den Wert von GetTariffCourse zurück
+*
+*/
 vector<float>  TariffData::GetNetworkLoadCourse(){
 	return networkLoadCourse;
 }
+/** Diese Methode gibt den Wert von GetTariffCourse als String zurück
+*
+*/
 string TariffData::GetNetworkLoadCourse_string(){
 	StringConverter converter;
 
