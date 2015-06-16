24d52ccb386c87792825c5067882e48fa431fa4f
burningT <tobias-fr.rastetter@gmx.de>
2015-06-16 09:08:16 +0200

    Server geupdated
    -Kommentare in Klasse StringConverter
    -überflüssige .ino gelöscht

--- SmartGridServer/SmartGridServer/StringConverter.cpp
+++ SmartGridServer/SmartGridServer/StringConverter.cpp
@@ -20,18 +20,20 @@ StringConverter::StringConverter(string delimiterL, string delimiterR, string se
 	StringConverter::separator = separator;
 }
 
+/**
+*	Diese Methode konvertiert einen Vektor in einen String
+*/
 string StringConverter::toString(vector<float> vector){
-
 
 	ostringstream oss;
 
 	if (!vector.empty()){
 
-		oss << delimiterL;
-
+		oss << delimiterL;	//Füge linken delimiter ein
+														//Füge Elemente mit Trennzeichen ein
 		copy(vector.begin(), vector.end() - 1, ostream_iterator<int>(oss, separator.c_str()));
 
-		oss << vector.back() << delimiterR;		//Füge letztes Element ohne Begrenzer ein
+		oss << vector.back() << delimiterR;		//Füge letztes Element mit rechtem delimiter ein
 	}
 
 	return oss.str();
